3e7511a22ba53aee7f49b6906a91e47b74850caa
Max Drayer <0864328@hr.nl>
2016-06-23 21:49:01 +0200

    extra shit

--- TestApp/app/src/main/java/com/example/onno/testapp/Main2Activity.java
+++ TestApp/app/src/main/java/com/example/onno/testapp/Main2Activity.java
@@ -74,6 +74,7 @@ public class Main2Activity extends AppCompatActivity {
             }
         });
 
+
         buttonLineChart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
