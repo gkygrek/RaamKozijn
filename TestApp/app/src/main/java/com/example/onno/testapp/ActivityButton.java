ac342a1c9d4e6aba52dc052d4e2b5a94b774af89
gkygrek <t.c.o.verhagen@gmail.com>
2016-06-23 14:12:37 +0200

    new chart menu design

---
+++ TestApp/app/src/main/java/com/example/onno/testapp/ActivityButton.java
@@ -0,0 +1,29 @@
+package com.example.onno.testapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
+/**
+ * Created by onno on 23-6-2016.
+ */
+public class ActivityButton {
+    String nameButton;
+    String nameActivity;
+    Context context;
+    public ActivityButton(String nameButton,String nameActivity)
+    {
+        this.nameButton = nameButton;
+        this.nameActivity = nameActivity;
+    }
+
+    context = getApplicationContext();
+    nameButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+
+        public void onClick(View view) {
+            Intent intent = new Intent(context,nameActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+
+}
