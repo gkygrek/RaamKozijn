e4178ae7138a0b08fd96577752c60b55b4fcc7d4
Max Drayer <0864328@hr.nl>
2016-06-23 15:21:22 +0200

    LineGraph

--- TestApp/app/src/main/java/com/example/onno/testapp/Main6Activity.java
+++ TestApp/app/src/main/java/com/example/onno/testapp/Main6Activity.java
@@ -7,6 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class Main6Activity extends AppCompatActivity {
 
     @Override
@@ -24,6 +28,16 @@ public class Main6Activity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
     }
 
 }
